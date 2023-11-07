8f699005e7be905a710eda8de2795a92093f0bdf
blessedbiello <blessedbiello@gmail.com>
2023-11-07 23:40:09 +0100

    nft image

--- programs/solana-nft-anchor/src/lib.rs
+++ programs/solana-nft-anchor/src/lib.rs
@@ -1,30 +1,89 @@
+// importing dependencies
 use anchor_lang::prelude::*;
 use anchor_spl::{
     associated_token::AssociatedToken,
-    token::{Mint, Token, TokenAccount}
+    metadata::{
+        create_master_edition_v3, create_metadata_accounts_v3, CreateMasterEditionV3,
+        CreateMetadataAccountsV3, Metadata,
+    },
+    token::{mint_to, Mint, MintTo, Token, TokenAccount},
+};
+use mpl_token_metadata::{
+    pda::{find_master_edition_account, find_metadata_account},
+    state::DataV2,
 };
 
+// program id declaration
 declare_id!("C8Rd2i4EFiKmJxAfucJvavFU7QBwK8Vb9MQCAxEabB5a");
 
-// #[program]
+#[program]
 pub mod solana_nft_anchor {
+
     use super::*;
 
-    pub fn init_nft(ctx: Context<InitNFT>) -> Result<()> {
+    // create a new token mint with pda as authority
+    pub fn init_nft(
+        ctx: Context<InitNFT>,
+        name: String,
+        symbol: String,
+        uri: String,
+    ) -> Result<()> {
         // create mint account
         let cpi_context = CpiContext::new(
             ctx.accounts.token_program.to_account_info(),
-            MintTo{
-                        mint:
-                ctx.accounts.mint.to_account_info(),
-                to:
-                ctx.accounts.associated_token_account.to_account_info(),
-                authority:
-                ctx.accounts.signer.to_account_info(),
+            MintTo {
+                mint: ctx.accounts.mint.to_account_info(),
+                to: ctx.accounts.associated_token_account.to_account_info(),
+                authority: ctx.accounts.signer.to_account_info(),
             },
         );
 
         mint_to(cpi_context, 1)?;
+
+        // create metadata account
+        let cpi_context = CpiContext::new(
+            ctx.accounts.token_metadata_program.to_account_info(),
+            CreateMetadataAccountsV3 {
+                metadata: ctx.accounts.metadata_account.to_account_info(),
+                mint: ctx.accounts.mint.to_account_info(),
+                mint_authority: ctx.accounts.signer.to_account_info(),
+                update_authority: ctx.accounts.signer.to_account_info(),
+                payer: ctx.accounts.signer.to_account_info(),
+                system_program: ctx.accounts.system_program.to_account_info(),
+                rent: ctx.accounts.rent.to_account_info(),
+            },
+        );
+
+        // initialize the data2 struct with the required nft parameters
+        let data_v2 = DataV2 {
+            name: name,
+            symbol: symbol,
+            uri: uri,
+            seller_fee_basis_points: 0,
+            creators: None,
+            collection: None,
+            uses: None,
+        };
+
+        create_metadata_accounts_v3(cpi_context, data_v2, false, true, None)?;
+
+        //create master edition account
+        let cpi_context = CpiContext::new(
+            ctx.accounts.token_metadata_program.to_account_info(),
+            CreateMasterEditionV3 {
+                edition: ctx.accounts.master_edition_account.to_account_info(),
+                mint: ctx.accounts.mint.to_account_info(),
+                update_authority: ctx.accounts.signer.to_account_info(),
+                mint_authority: ctx.accounts.signer.to_account_info(),
+                payer: ctx.accounts.signer.to_account_info(),
+                metadata: ctx.accounts.metadata_account.to_account_info(),
+                token_program: ctx.accounts.token_program.to_account_info(),
+                system_program: ctx.accounts.system_program.to_account_info(),
+                rent: ctx.accounts.rent.to_account_info(),
+            },
+        );
+
+        create_master_edition_v3(cpi_context, None)?;
 
         Ok(())
     }
@@ -32,9 +91,9 @@ pub mod solana_nft_anchor {
 
 #[derive(Accounts)]
 pub struct InitNFT<'info> {
-    /// CHECK: ok, we are passing in this account by ourselves
+    /// CHECK: ok, we are passing in this account ourselves
     #[account(mut, signer)]
-    signer: AccountInfo<'info>,
+    pub signer: AccountInfo<'info>,
     #[account(
         init,
         payer = signer,
@@ -42,18 +101,32 @@ pub struct InitNFT<'info> {
         mint::authority = signer.key(),
         mint::freeze_authority = signer.key(),
     )]
-    pub mint: Account<'info,  Mint>,
+    pub mint: Account<'info, Mint>,
     #[account(
         init_if_needed,
         payer = signer,
         associated_token::mint = mint,
-        associated_token::authority = signer,
+        associated_token::authority = signer
     )]
     pub associated_token_account: Account<'info, TokenAccount>,
 
+    /// CHECK - address
+    #[account(
+        mut,
+        address=find_metadata_account(&mint.key()).0,
+    )]
+    pub metadata_account: AccountInfo<'info>,
+    
+    /// CHECK: address
+    #[account(
+        mut,
+        address=find_master_edition_account(&mint.key()).0,
+    )]
+    pub master_edition_account: AccountInfo<'info>,
+
     pub token_program: Program<'info, Token>,
     pub associated_token_program: Program<'info, AssociatedToken>,
+    pub token_metadata_program: Program<'info, Metadata>,
     pub system_program: Program<'info, System>,
-    pub rent: Sysvar<'info, Rent>
-
+    pub rent: Sysvar<'info, Rent>,
 }
--- programs/solana-nft-anchor/src/lib2.rs
+++ programs/solana-nft-anchor/src/lib2.rs
@@ -1,16 +1,52 @@
 use anchor_lang::prelude::*;
 use anchor_spl::{
     associated_token::AssociatedToken,
-    token::{Mint, Token, TokenAccount},
+    metadata::{
+        create_master_edition_v3, create_metadata_accounts_v3, CreateMasterEditionV3,
+        CreateMetadataAccountsV3, Metadata,
+    },
+    token::{mint_to, Mint, MintTo, Token, TokenAccount},
 };
 
-// snip
+use mpl_token_metadata::{
+    pda::{find_master_edition_account, find_metadata_account},
+    state::DataV2,
+};
+
+declare_id!("C8Rd2i4EFiKmJxAfucJvavFU7QBwK8Vb9MQCAxEabB5a");
+
+// #[program]
+pub mod solana_nft_anchor {
+    use super::*;
+
+    pub fn init_nft(
+        ctx: Context<InitNFT>,
+        name: String,
+        symbol: String,
+        uri: String
+    ) -> Result<()> {
+        // create mint account
+        let cpi_context = CpiContext::new(
+            ctx.accounts.token_program.to_account_info(),
+            MintTo {
+                mint: ctx.accounts.mint.to_account_info(),
+                to: ctx.accounts.associated_token_account.to_account_info(),
+                authority: ctx.accounts.signer.to_account_info(),
+            },
+        );
+
+        mint_to(cpi_context, 1)?;
+
+        Ok(())
+
+    }
+}
 
 #[derive(Accounts)]
 pub struct InitNFT<'info> {
-    /// CHECK: ok, we are passing in this account ourselves
+    /// CHECK: ok, we are passing in this account by ourselves
     #[account(mut, signer)]
-    pub signer: AccountInfo<'info>,
+    signer: AccountInfo<'info>,
     #[account(
         init,
         payer = signer,
@@ -18,17 +54,35 @@ pub struct InitNFT<'info> {
         mint::authority = signer.key(),
         mint::freeze_authority = signer.key(),
     )]
-    pub mint: Account<'info, Mint>, // new
+    pub mint: Account<'info, Mint>,
     #[account(
         init_if_needed,
         payer = signer,
         associated_token::mint = mint,
         associated_token::authority = signer,
     )]
-    pub associated_token_account: Account<'info, TokenAccount>, // new
-
-    pub token_program: Program<'info, Token>, // new
-    pub associated_token_program: Program<'info, AssociatedToken>, // new
-    pub system_program: Program<'info, System>, // bew
-    pub rent: Sysvar<'info, Rent> // new
+    pub metadata_account: AccountInfo<'info>,
+    /// CHECK:address
+    #[account(
+        mut,
+        address=find_master_edition_account(&mint.key()).0,
+    )]
+    // pub associated_token_account: Account<'info, TokenAccount>,
+    pub token_program: Program<'info, Token>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub system_program: Program<'info, System>,
+    pub rent: Sysvar<'info, Rent>,
 }
+
+
+
+
+
+
+
+
+
+
+// [provider]
+// cluster = "Localnet"
+// wallet = "/home/bprime/.config/solana/id.json"
